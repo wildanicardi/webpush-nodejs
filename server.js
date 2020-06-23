5e8f43268a5f4dfc5db8452c0fa09d3367d9716d
wildanicardi <wildanicardi9@gmail.com>
2020-06-23 22:03:01 +0700

    update route

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const path = require('path');
+const path = require("path");
 const bodyParser = require("body-parser");
 const port = 8000;
 const app = express();
-const keys = require('./configuration/keys');
+const keys = require("./configuration/keys");
 
 app.use(bodyParser.json());
 // app.use(express.static(path.join(__dirname, 'public')));
@@ -13,65 +13,57 @@ app.listen(port, () => {
   console.log(`server is listening on port:${port}`);
 });
 
-mongoose.connect(keys.mongoURI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-}).then(() => console.log("MongoDB Connected")).catch((err) => console.log(err));
+mongoose
+  .connect(keys.mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log(err));
 
-const passport = require('passport');
-const passportConf = require('./passport');
-const passportLogin = passport.authenticate('local', {
-  session: false
+const passport = require("passport");
+const passportConf = require("./passport");
+const passportLogin = passport.authenticate("local", {
+  session: false,
 });
-const passportJwt = passport.authenticate('jwt', {
-  session: false
+const passportJwt = passport.authenticate("jwt", {
+  session: false,
 });
-const {
-  signup,
-  login
-} = require('./controller/Auth');
+const { signup, login } = require("./controller/Auth");
 const {
   index,
   deleteUser,
   updateUser,
   showUser,
-  createUser
-} = require('./controller/Users');
-const {
-  createRole,
-  indexRole
-} = require("./controller/Role");
-const {
-  indexSubscriber,
-  getSubscriber
-} = require("./controller/Subscribe");
-const {
-  indexPush
-} = require("./controller/Push");
+  createUser,
+} = require("./controller/Users");
+const { createRole, indexRole } = require("./controller/Role");
+const { indexSubscriber, getSubscriber } = require("./controller/Subscribe");
+const { indexPush } = require("./controller/Push");
 // const Subscribe = require("./models/Subscriber");
 //login
-app.post("/auth/login", passportLogin, login);
+app.post("/api/auth/login", passportLogin, login);
 // register
-app.post("/auth/signup", signup);
+app.post("/api/auth/signup", signup);
 // CREATE
-app.post("/users", createUser);
-app.get("/users", passportJwt, index);
+app.post("/api/users", createUser);
+app.get("/api/users", passportJwt, index);
 app
-  .route("/users/:id")
+  .route("/api/users/:id")
   // READ
   .get(passportJwt, showUser)
   // UPDATE
   .put(passportJwt, updateUser)
   // DELETE
   .delete(passportJwt, deleteUser);
-app.post("/roles", passportJwt, createRole);
-app.get("/roles", passportJwt, indexRole);
+app.post("/api/roles", passportJwt, createRole);
+app.get("/api/roles", passportJwt, indexRole);
 app.get("/", (req, res) => {
   res.send("Hello World");
-})
+});
 
 app.get("/subscribe", passportJwt, getSubscriber);
 app.post("/subscribe", passportJwt, indexSubscriber);
 
-app.post("/push", passportJwt, indexPush);
+app.post("/api/push", passportJwt, indexPush);
